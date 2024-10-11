488b6536a28b3989347d0cc4bf60671b20c1a1f8
pgf3712 <pgf3712@gmail.com>
2024-10-11 16:08:02 +0200

    Merge branch 'Grupo-Rubén,-Hugo,-Paula' of https://github.com/Diego-nunez-TA/taller_GitHub into Grupo-Rubén,-Hugo,-Paula

--- Practica/scrapper_ruben_hugo_paula.py
+++ Practica/scrapper_ruben_hugo_paula.py
@@ -1,13 +1,96 @@
+<<<<<<< HEAD
 MAIN
 
 
 # PAULA
 
 url = "https://es.wikipedia.org/wiki/Anexo:Libros_m%C3%A1s_vendidos" 
+=======
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>>>>>>> f2f28b737ff5cdd8d4e2095c9ec25633cff7547d
 
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
+<<<<<<< HEAD
 import numpy as np
 import requests
 from fake_useragent import UserAgent
@@ -15,3 +98,5 @@ ua = UserAgent()
 headers = {'User-Agent': ua.random}
 response = requests.get(url, headers=headers)
 print(response)
+=======
+>>>>>>> 897a18b9ab0a64d88dffe242f5274e72cad5a1be
