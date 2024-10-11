00146f3675a1926510b335bee00c07547809ef77
pgf3712 <pgf3712@gmail.com>
2024-10-11 15:28:21 +0200

    PAULA

--- Practica/scrapper_ruben_hugo_paula.py
+++ Practica/scrapper_ruben_hugo_paula.py
@@ -1,1 +1,17 @@
 MAIN
+
+
+# PAULA
+
+url = "https://es.wikipedia.org/wiki/Anexo:Libros_m%C3%A1s_vendidos" 
+
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+import numpy as np
+import requests
+from fake_useragent import UserAgent
+ua = UserAgent()
+headers = {'User-Agent': ua.random}
+response = requests.get(url, headers=headers)
+print(response)
