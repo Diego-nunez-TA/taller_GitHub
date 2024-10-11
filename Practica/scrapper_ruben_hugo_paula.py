c266facfef1d8be1e7fcaaa10f70c84871165ca2
hug4470 <hugomenendez4470@gmail.com>
2024-10-11 15:21:43 +0200

    Prueba commit

--- Practica/scrapper_ruben_hugo_paula.py
+++ Practica/scrapper_ruben_hugo_paula.py
@@ -1,1 +1,19 @@
-MAIN
+# from bs4 import BeautifulSoup as bs
+# import requests
+# import pandas as pd
+# # from splinter import Browser
+# import numpy as np
+
+# pd.set_option('max_colwidth', 800)
+
+# url = "https://es.wikipedia.org/wiki/Anexo:Campeones_de_la_NBA"
+# response = requests.get(url)
+
+# print(response)
+
+# html = response.content
+
+# soup = bs(html, 'html.parser')
+
+# for x in soup.find_all('a', href=<a href="/wiki/Boston_Celtics"):
+#     print(x.get_text())
