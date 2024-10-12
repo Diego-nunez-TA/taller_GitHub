066c96ac317e6a4607ec209b406c004700546f10
RauGarGom <raul.garciagomez@hotmail.com>
2024-10-12 11:30:18 +0200

    RGG 12/10 - aplicación de código en utils y create_csv

--- equipo_wildcat/create_csv.py
+++ equipo_wildcat/create_csv.py
@@ -1,5 +1,5 @@
-### Raúl - ¿qué opináis de aunar sección de librerías??
-import pandas as pd
+### Import utils
+import utils as ut
 
 # Manuel
 
@@ -8,20 +8,4 @@ import pandas as pd
 # Alejandro
 
 # Raúl
-def nbachamps_to_csv():
-    print("Conectando a web...")
-    datanba = pd.read_html('https://es.wikipedia.org/wiki/Anexo:Campeones_de_la_NBA', index_col=0)
-
-    ### read_html devuelve una **lista de dataframes***. Esto de aquí abajo busca dónnde está el nombre de  
-    ### una de las columnas,y devuelve aquella donde lo encuentra
-    lookup_str = "Años campeón"
-    nba_champs = next ((table for table in datanba if lookup_str in table.stack().to_string()))
-
-    ###Terminamos printeando el df en el csv final
-    print("Creando csv...")
-    nba_champs.to_csv("rgg_nba_champs.csv")
-    print("¡Terminado!")
-
-
-### Raúl - Si vamos todos a funciones, ¿qué opináis de ejecutarlas todas aquí abajo?
-nbachamps_to_csv()
+ut.nbachamps_to_csv()
---
+++ equipo_wildcat/utils.py
@@ -0,0 +1,17 @@
+### Librerías
+import pandas as pd
+
+### Raúl
+def nbachamps_to_csv():
+    print("Conectando a web...")
+    datanba = pd.read_html('https://es.wikipedia.org/wiki/Anexo:Campeones_de_la_NBA', index_col=0)
+
+    ### read_html devuelve una **lista de dataframes***. Esto de aquí abajo busca dónnde está el nombre de  
+    ### una de las columnas,y devuelve aquella donde lo encuentra
+    lookup_str = "Años campeón"
+    nba_champs = next ((table for table in datanba if lookup_str in table.stack().to_string()))
+
+    ###Terminamos printeando el df en el csv final
+    print("Creando csv...")
+    nba_champs.to_csv("rgg_nba_champs.csv")
+    print("¡Terminado!")
