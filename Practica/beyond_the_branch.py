15d566a11ccafd17eeed041ee4333d25e0f89604
ndjo-byte <jonesn.96@gmail.com>
2024-10-12 20:17:18 +0200

    nathan, función scrape de top10libros

--- Practica/beyond_the_branch.py
+++ Practica/beyond_the_branch.py
@@ -12,3 +12,11 @@ soup = BeautifulSoup(page.content, 'html.parser')
 tbl = soup.find_all("table",{"class": "wikitable"})
 nba_df = pd.read_html(StringIO(str(tbl[4])))[0]
 nba_df.to_csv("nba_rings.csv")
+
+#función scrape: Top10Libros
+def top_books_scrape():
+    url = 'https://en.wikipedia.org/wiki/List_of_best-selling_books'
+    response1 = pd.read_html(url, header=0)[0]
+    response2 = pd.read_html(url, header=0)[1]
+    top_10_books = pd.concat([response1.iloc[:,[0,1,3]], response2.iloc[0:2, [0,1,3]]], ignore_index=True)
+    top_10_books.to_csv('top_10_wiki_books.csv')
